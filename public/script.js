3498d70608c58d40bb192fe7b51210fee47d039f
Glitch (hello-express) <none>
2020-03-31 20:54:16 +0000

    🌇👹 Checkpoint ./src/something.test.js:7701387/1104 ./public/script.js:7701387/65

--- public/script.js
+++ public/script.js
@@ -2,6 +2,12 @@ async function loadResults() {
   const results = await fetch("/results.json");
   const body = await results.json();
   console.log(body);
+  
+  
+  for (const fileResult of body.testResults) {
+    
+  }
+  
 }
 
 loadResults();
---
+++ src/something.test.js
@@ -0,0 +1,38 @@
+describe("Desc 1", () => {
+  
+  it("Desc 1 - Test 1", () => {
+    const a = 1;
+    const b = 1;
+    
+    expect(a).toBe(b);
+  });
+  
+  describe("Desc 1 - Desc 2", () => {
+      
+    it("Desc 1 - Desc 2 - Test 1", () => {
+      const a = 1;
+      const b = 1;
+
+      expect(a).toBe(b);
+      expect(b).toBe(a);
+    });
+    
+    it("Desc 1 - Desc 2 - Test 2", () => {
+      const a = 1;
+      const b = 1;
+
+      expect(a).toBe(b);
+      expect(b).toBe(a);
+    });    
+  });
+  
+    
+  it("Desc 1 - Test 2", () => {
+    const a = 1;
+    const b = 1;
+    
+    expect(a).toBe(b);
+  });
+  
+  
+});
